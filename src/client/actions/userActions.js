034e0c98f1e5e180b2c647036d0cf8494ed9c69b
Tanwinder Sidhu <sidhu@Gurpreets-Air.attlocal.net>
2020-11-02 22:51:01 -0500

    add proxy code for passing cookie's

--- src/client/actions/userActions.js
+++ src/client/actions/userActions.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 import {FETCH_USERS} from '../types';
 
-export const fetchUsers = () => async dispatch => {
+export const fetchUsers = () => async (dispatch, getState, api) => {
     //http://react-ssr-api.herokuapp.com/users/xss  to check safe from XSS attack
-    const res = await axios.get("http://react-ssr-api.herokuapp.com/users");
+    //http://react-ssr-api.herokuapp.com/users for users
+    const res = await api.get("/users");
 
     console.log("response", res)
 
--- src/helpers/createStore.js
+++ src/helpers/createStore.js
@@ -1,9 +1,17 @@
 import { createStore, applyMiddleware } from 'redux';
-import reducers from '../client/reducers/index'
 import thunk from 'redux-thunk';
+import axios from 'axios';
+import reducers from '../client/reducers/index';
 
-export default () => {
-    const store = createStore(reducers, {}, applyMiddleware(thunk));
+export default (req) => {
+    const axiosInstance = axios.create({
+        baseURL: 'http://react-ssr-api.herokuapp.com',
+        headers: {cookie: req.get('cookie') || ''}
+    })
+    const store = createStore(
+        reducers, 
+        {},
+        applyMiddleware(thunk.withExtraArgument(axiosInstance)));
 
     return store;
 }
