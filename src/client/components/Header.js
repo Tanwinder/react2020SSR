36c1e7ce7414d7a6011c79a12f66099ede47e71d
Tanwinder Sidhu <sidhu@Gurpreets-Air.attlocal.net>
2020-11-06 21:46:20 -0500

    add header change

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -12,7 +12,7 @@ const Header = ({auth}) => {
     return (
         <nav>
             <div className="nav-wrapper">
-            <a href="#" className="brand-logo">React SSR App</a>
+            <Link to="/" className="brand-logo">React SSR App</Link>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
                 <li><Link to="/users">Users</Link></li>
                 <li><Link to="/admins">Admins</Link></li>
